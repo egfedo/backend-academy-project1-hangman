cfdd13646be83c4be43fc570ef2e13d1dcc2818f
egfedo <fegor04@gmail.com>
2024-09-13 19:42:46 +0300

    refactor: remove unused import

--- src/test/java/backend/academy/egfedo/data/WordTest.java
+++ src/test/java/backend/academy/egfedo/data/WordTest.java
@@ -3,7 +3,6 @@ package backend.academy.egfedo.data;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class WordTest {
 
