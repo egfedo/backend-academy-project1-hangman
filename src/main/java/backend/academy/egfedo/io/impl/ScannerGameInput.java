fef061a6a862e3e49ecc5d2aa9e55122a710e754
egfedo <fegor04@gmail.com>
2024-09-25 22:02:57 +0300

    feat: add ScannerGameInput

---
+++ src/main/java/backend/academy/egfedo/io/impl/ScannerGameInput.java
@@ -0,0 +1,23 @@
+package backend.academy.egfedo.io.impl;
+
+import backend.academy.egfedo.io.GameInput;
+import java.util.Objects;
+import java.util.Scanner;
+
+public final class ScannerGameInput implements GameInput {
+
+    private final Scanner scanner;
+
+    public ScannerGameInput(Scanner scanner) {
+        this.scanner = Objects.requireNonNull(scanner);
+    }
+
+    @Override
+    public Command getCommand() {
+        String input = scanner.nextLine();
+        return new Command(
+            input.length() == 1 ? Command.Type.CHAR : Command.Type.STRING,
+            input
+        );
+    }
+}
