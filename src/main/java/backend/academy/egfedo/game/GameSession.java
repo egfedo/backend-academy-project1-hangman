06b02680927f2ac2757b64060c0da4e28dfce05e
egfedo <fegor04@gmail.com>
2024-09-25 23:51:52 +0300

    feat: add game result output to the GameSession

--- src/main/java/backend/academy/egfedo/game/GameSession.java
+++ src/main/java/backend/academy/egfedo/game/GameSession.java
@@ -2,6 +2,7 @@ package backend.academy.egfedo.game;
 
 import backend.academy.egfedo.game.menu.GameMenu;
 import backend.academy.egfedo.game.misc.LevelSupplier;
+import backend.academy.egfedo.io.ResultOutput;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,10 +12,12 @@ public final class GameSession {
 
     private final List<GameMenu> menuList;
     private final LevelSupplier levelSupplier;
+    private final ResultOutput resultOutput;
 
-    public GameSession(List<GameMenu> menuList, LevelSupplier levelSupplier) {
+    public GameSession(List<GameMenu> menuList, LevelSupplier levelSupplier, ResultOutput resultOutput) {
         this.menuList = Objects.requireNonNull(menuList, "menuList cannot be null");
         this.levelSupplier = Objects.requireNonNull(levelSupplier, "levelSupplier cannot be null");
+        this.resultOutput = Objects.requireNonNull(resultOutput, "resultOutput cannot be null");
 
         if (menuList.stream().anyMatch(Objects::isNull)) {
             throw new IllegalArgumentException("menuList cannot contain null values");
@@ -30,6 +33,11 @@ public final class GameSession {
         }
 
         var level = levelSupplier.getLevel(gameOptions);
-        level.run();
+
+        boolean resultFlag = level.run();
+        ResultOutput.Result result = resultFlag ? ResultOutput.Result.WIN : ResultOutput.Result.LOSE;
+        String message = resultFlag ? "Хорошая работа!" : "Было загадано слово: " + level.word().toUpperCase();
+        resultOutput.outputResult(result, message);
+
     }
 }
