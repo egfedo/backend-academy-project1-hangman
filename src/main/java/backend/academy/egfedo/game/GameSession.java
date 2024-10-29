3b3d4b37dfa2b2571d1ad2a68f9cec214e38c1ba
egfedo <fegor04@gmail.com>
2024-10-29 17:44:10 +0300

    refactor: change GameSession to work with enums

--- src/main/java/backend/academy/egfedo/game/GameSession.java
+++ src/main/java/backend/academy/egfedo/game/GameSession.java
@@ -1,5 +1,6 @@
 package backend.academy.egfedo.game;
 
+import backend.academy.egfedo.data.WordRegistry;
 import backend.academy.egfedo.game.menu.GameMenu;
 import backend.academy.egfedo.game.misc.LevelSupplier;
 import backend.academy.egfedo.io.ResultOutput;
@@ -10,29 +11,27 @@ import java.util.Objects;
 
 public final class GameSession {
 
-    private final List<GameMenu> menuList;
+    private final GameMenu<WordRegistry.Category> categoryMenu;
+    private final GameMenu<WordRegistry.Difficulty> difficultyMenu;
     private final LevelSupplier levelSupplier;
     private final ResultOutput resultOutput;
 
-    public GameSession(List<GameMenu> menuList, LevelSupplier levelSupplier, ResultOutput resultOutput) {
-        this.menuList = Objects.requireNonNull(menuList, "menuList cannot be null");
+    public GameSession(GameMenu<WordRegistry.Category> categoryMenu,
+        GameMenu<WordRegistry.Difficulty> difficultyMenu,
+        LevelSupplier levelSupplier, ResultOutput resultOutput) {
+        this.categoryMenu = Objects.requireNonNull(categoryMenu, "categoryMenu cannot be null");
+        this.difficultyMenu = Objects.requireNonNull(difficultyMenu, "difficultyMenu cannot be null");
         this.levelSupplier = Objects.requireNonNull(levelSupplier, "levelSupplier cannot be null");
         this.resultOutput = Objects.requireNonNull(resultOutput, "resultOutput cannot be null");
-
-        if (menuList.stream().anyMatch(Objects::isNull)) {
-            throw new IllegalArgumentException("menuList cannot contain null values");
-        }
 
     }
 
     public void run() {
-        Map<GameMenu.OptionType, String> gameOptions = new HashMap<>();
 
-        for (GameMenu menu : menuList) {
-            menu.run(gameOptions);
-        }
+        WordRegistry.Category category = categoryMenu.run();
+        WordRegistry.Difficulty difficulty = difficultyMenu.run();
 
-        var level = levelSupplier.getLevel(gameOptions);
+        var level = levelSupplier.getLevel(category, difficulty);
 
         boolean resultFlag = level.run();
         ResultOutput.Result result = resultFlag ? ResultOutput.Result.WIN : ResultOutput.Result.LOSE;
