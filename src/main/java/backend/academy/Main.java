4f96592f57b25c14261ca8de942258e69deb59d1
egfedo <fegor04@gmail.com>
2024-10-29 18:06:56 +0300

    refactor: move game setup to a separate utility class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,8 +1,10 @@
 package backend.academy;
 
 import backend.academy.egfedo.config.io.LevelPrintStreamOutputConfig;
-import backend.academy.egfedo.data.Word;
+import backend.academy.egfedo.data.Category;
+import backend.academy.egfedo.data.Difficulty;
 import backend.academy.egfedo.data.WordRegistry;
+import backend.academy.egfedo.game.Game;
 import backend.academy.egfedo.game.GameSession;
 import backend.academy.egfedo.game.menu.GameMenu;
 import backend.academy.egfedo.game.misc.LevelSupplier;
@@ -11,7 +13,6 @@ import backend.academy.egfedo.io.impl.MenuPrintStreamOutput;
 import backend.academy.egfedo.io.impl.ResultPrintStreamOutput;
 import backend.academy.egfedo.io.impl.ScannerGameInput;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 import java.util.Scanner;
 import lombok.experimental.UtilityClass;
 
@@ -20,39 +21,6 @@ public class Main {
 
     @SuppressWarnings({"MultipleStringLiterals"})
     public static void main(String[] args) {
-
-        var config = new LevelPrintStreamOutputConfig("./config/output_config.yml").getData();
-        var menuOutput = new MenuPrintStreamOutput(System.out);
-        var levelOutput = new LevelPrintStreamOutput(System.out, config);
-        var resultOutput = new ResultPrintStreamOutput(System.out);
-
-        var scanner = new Scanner(System.in, StandardCharsets.UTF_8);
-        var gameInput = new ScannerGameInput(scanner);
-
-        var registry = new WordRegistry();
-        var levelSupplier = new LevelSupplier(levelOutput, gameInput, registry);
-
-        GameMenu<WordRegistry.Category> catMenu = new GameMenu<>(
-            "Выберите категорию:",
-            menuOutput,
-            gameInput,
-            WordRegistry.Category.class
-        );
-
-        GameMenu<WordRegistry.Difficulty> diffMenu = new GameMenu<>(
-            "Выберите сложность:",
-            menuOutput,
-            gameInput,
-            WordRegistry.Difficulty.class
-        );
-
-        var gameSession = new GameSession(
-            catMenu,
-            diffMenu,
-            levelSupplier,
-            resultOutput
-        );
-
-        gameSession.run();
+        Game.setUpSession();
     }
 }
---
+++ src/main/java/backend/academy/egfedo/game/Game.java
@@ -0,0 +1,55 @@
+package backend.academy.egfedo.game;
+
+import backend.academy.egfedo.config.io.LevelPrintStreamOutputConfig;
+import backend.academy.egfedo.data.Category;
+import backend.academy.egfedo.data.Difficulty;
+import backend.academy.egfedo.data.WordRegistry;
+import backend.academy.egfedo.game.menu.GameMenu;
+import backend.academy.egfedo.game.misc.LevelSupplier;
+import backend.academy.egfedo.io.impl.LevelPrintStreamOutput;
+import backend.academy.egfedo.io.impl.MenuPrintStreamOutput;
+import backend.academy.egfedo.io.impl.ResultPrintStreamOutput;
+import backend.academy.egfedo.io.impl.ScannerGameInput;
+import lombok.experimental.UtilityClass;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+@UtilityClass
+public class Game {
+
+    public static void setUpSession() {
+        var config = new LevelPrintStreamOutputConfig("./config/output_config.yml").getData();
+        var menuOutput = new MenuPrintStreamOutput(System.out);
+        var levelOutput = new LevelPrintStreamOutput(System.out, config);
+        var resultOutput = new ResultPrintStreamOutput(System.out);
+
+        var scanner = new Scanner(System.in, StandardCharsets.UTF_8);
+        var gameInput = new ScannerGameInput(scanner);
+
+        var registry = new WordRegistry();
+        var levelSupplier = new LevelSupplier(levelOutput, gameInput, registry);
+
+        GameMenu<Category> catMenu = new GameMenu<>(
+            "Выберите категорию:",
+            menuOutput,
+            gameInput,
+            Category.class
+        );
+
+        GameMenu<Difficulty> diffMenu = new GameMenu<>(
+            "Выберите сложность:",
+            menuOutput,
+            gameInput,
+            Difficulty.class
+        );
+
+        var gameSession = new GameSession(
+            catMenu,
+            diffMenu,
+            levelSupplier,
+            resultOutput
+        );
+
+        gameSession.run();
+    }
+}
